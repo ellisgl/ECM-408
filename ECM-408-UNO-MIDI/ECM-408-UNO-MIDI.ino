47a62d247f9ced27b67b5ff91c837be8f7b7f2b8
ellisgl <xelliss@gmail.com>
2018-05-07 01:35:04 -0500

    Had to adjust button mapping a little bit. MIDI note 11 was just too low, so changed it to start from 12.

--- ECM-408-UNO-MIDI/ECM-408-UNO-MIDI.ino
+++ ECM-408-UNO-MIDI/ECM-408-UNO-MIDI.ino
@@ -16,10 +16,8 @@ Bounce b4 = Bounce();
 uint8_t c = 0; // Column counter
 
 // Button Mapping
-// Based on Novation Launchpad MK2 mapping:
-// https://global.novationmusic.com/sites/default/files/novation/downloads/10529/launchpad-mk2-programmers-reference-guide_0.pdf
 const uint8_t bMap[40] = {
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 29,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 29,
     21, 22, 23, 24, 25, 26, 27, 28, 39, 49,
     31, 32, 33, 34, 35, 36, 37, 38, 59, 69,
     41, 42, 43, 44, 45, 46, 47, 48, 79, 89
